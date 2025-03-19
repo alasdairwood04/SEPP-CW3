0aeecefb8cd210e2be540652636b7c44191af1a3
alasdairwood04 <alasdair.wood9@btinternet.com>
2025-03-19 10:43:30 +0000

    Adding course tag to constructor and hasTag method

--- src/main/java/model/FAQItem.java
+++ src/main/java/model/FAQItem.java
@@ -3,12 +3,19 @@ package model;
 public class FAQItem {
     private final String question;
     private final String answer;
+    private final String courseTag;
 
     public FAQItem(String question, String answer) {
         this.question = question;
         this.answer = answer;
+        this.courseTag = null;
     }
 
+    public FAQItem(String question, String answer, String courseTag) {
+        this.question = question;
+        this.answer = answer;
+        this.courseTag = courseTag;
+    }
     public String getQuestion() {
         return question;
     }
@@ -16,4 +23,13 @@ public class FAQItem {
     public String getAnswer() {
         return answer;
     }
+
+    public String getCourseTag() {
+        return courseTag;
+    }
+
+    public boolean hasTag(String courseTag) {
+        return this.courseTag != null && this.courseTag.equals(courseTag);
+    }
 }
+
