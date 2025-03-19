92a52d6e426c79a1a297dd78d6a70062478bf670
alasdairwood04 <alasdair.wood9@btinternet.com>
2025-03-19 12:23:54 +0000

    wrote the basic method declorations for this class - havent implemented yet

---
+++ src/main/java/model/FAQManager.java
@@ -0,0 +1,20 @@
+package model;
+
+import java.util.*;
+import model.*;
+
+public class FAQManager {
+    public FAQManager() {}
+
+    public void addSection(String topic){
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public void removeSection(String topic){
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    private void promoteSection(FAQSection section){
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+}
