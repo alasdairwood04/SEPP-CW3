7631b798b91a12236ebb15a7bb939a82e28dfc30
alasdairwood04 <alasdair.wood9@btinternet.com>
2025-03-24 15:30:11 +0000

    Implemented basic functionality for this. FAQManager replaces FAQ class

--- src/main/java/model/FAQManager.java
+++ src/main/java/model/FAQManager.java
@@ -4,17 +4,29 @@ import java.util.*;
 import model.*;
 
 public class FAQManager {
-    public FAQManager() {}
+    private final List<FAQSection> sections = new ArrayList<>();
 
-    public void addSection(String topic){
-        throw new UnsupportedOperationException("Not implemented yet");
+    public FAQManager() {
     }
 
+    public void addSection(String topic){
+        FAQSection section = new FAQSection(topic);
+        sections.add(section);
+        section.setParent(null);
+    }
+
+    // dont think i need to implement this since team size of 3
     public void removeSection(String topic){
         throw new UnsupportedOperationException("Not implemented yet");
     }
 
+    // dont think i need to implement this since team size of 3
     private void promoteSection(FAQSection section){
         throw new UnsupportedOperationException("Not implemented yet");
     }
+
+    public List<FAQSection> getSections() {
+        return sections;
+    }
+
 }
