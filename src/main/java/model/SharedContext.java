e56ca2982003621a64db33275048a53b0f6a6156
alasdairwood04 <alasdair.wood9@btinternet.com>
2025-03-24 16:58:49 +0000

    removed no longer needed aspects of SharedContext

--- src/main/java/model/SharedContext.java
+++ src/main/java/model/SharedContext.java
@@ -8,36 +8,15 @@ public class SharedContext {
 
     public final List<Inquiry> inquiries;
     public final FAQ faq;
-    private final Map<String, Set<String>> faqTopicsUpdateSubscribers;
 
     public SharedContext() {
         this.currentUser = new Guest();
         this.inquiries = new ArrayList<>();
         faq = new FAQ();
-        faqTopicsUpdateSubscribers = new HashMap<>();
     }
 
     public FAQ getFAQ() {
         return faq;
-    }
-
-    public boolean registerForFAQUpdates(String email, String topic) {
-        if (faqTopicsUpdateSubscribers.containsKey(topic)) {
-            return faqTopicsUpdateSubscribers.get(topic).add(email);
-        } else {
-            Set<String> subscribers = new HashSet<>();
-            subscribers.add(email);
-            faqTopicsUpdateSubscribers.put(topic, subscribers);
-            return true;
-        }
-    }
-
-    public boolean unregisterForFAQUpdates(String email, String topic) {
-        return faqTopicsUpdateSubscribers.getOrDefault(topic, new HashSet<>()).remove(email);
-    }
-
-    public Set<String> usersSubscribedToFAQTopic(String topic) {
-        return faqTopicsUpdateSubscribers.getOrDefault(topic, new HashSet<>());
     }
 
     // TODO implement this method
